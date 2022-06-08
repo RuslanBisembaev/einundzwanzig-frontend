26a1311c0cc638be2e529081f1e9c97bff695259
RuslanBisembaev <rusl.bisembaev@gmail.com>
2022-06-08 10:31:28 +0200

    Hello World

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Einundzwanzig</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <router-link to="/">Home</router-link>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav-bar'
+}
+</script>
+
+<style scoped>
+
+</style>
